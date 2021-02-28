6994861f372f8194dce043af7409fac7ed4c60d1
moshe-gr <mosegreenberg6@gmail.com>
2021-02-28 22:46:00 +0200

    more

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const NUM = window.document.getElementById("num")
+const SCREEN = window.document.getElementById("screen")
+const BORD = window.document.getElementById("bord")
+const CARD = window.document.getElementById("cards")
+const TIME = window.document.getElementById("time")
+const MOVES = window.document.getElementById("moves")
+const ENDGAME = window.document.getElementById("endGame")
 var nums = []
 var opendCards = []
 var pairs = 0
@@ -6,19 +11,13 @@ var minuts = 0
 var seconds = 0
 var m0 = "0"
 var s0 = "0"
-var pause = true
 var start = null
 
+setNumbers()
+setBord()
+
 function startGame(){
-    opendCards.length = 0
-    minuts = 0
-    seconds = 0
-    m0 = "0"
-    s0 = "0"
-    gamePause()
-    setNumbers()
-    setBord()
-    startTimer()
+    window.document.location.reload()
 }
 function setNumbers(){
     nums.length = 0
@@ -31,10 +30,10 @@ function setNumbers(){
     }
 }
 function setBord(){
-    NUM.innerHTML = null
-    window.document.getElementById("num").className = "row bg-warning"
+    CARD.style.pointerEvents = "none"
+    CARD.className = "row bg-warning"
     for(let i = 0; i < nums.length; i++){
-        NUM.innerHTML += `<div id="${i}" class="col-3 my-2"><img src="images/blank.png" width="100%" onclick="show(${i})"></div>`
+        CARD.innerHTML += `<div id="${i}" class="col-3 my-2"><img src="images/blank.png" width="100%" onclick="show(${i})"></div>`
     }
 }
 function show(num){
@@ -44,12 +43,12 @@ function show(num){
     }, 50)
     opendCards[opendCards.length] = num
     if(opendCards.length % 2 == 0){
-        window.document.getElementById("moves").innerText = opendCards.length/2
+        MOVES.innerText = opendCards.length/2
         if(nums[opendCards[opendCards.length-2]] != nums[opendCards[opendCards.length-1]]){
-            NUM.style.pointerEvents = "none"
+            CARD.style.pointerEvents = "none"
             setTimeout(() => {
                 close();
-                NUM.style.pointerEvents = "auto";
+                CARD.style.pointerEvents = "auto";
             }, 700)
         }
         else{
@@ -69,16 +68,45 @@ function close(){
     }
 }
 function endGame(){
-    window.document.getElementById("num").className = "row bg-success"
+    gamePause()
+    SCREEN.style.pointerEvents = "none"
+    SCREEN.style.opacity = 0.3
+    var grow = 0
+    var shrink = 100
+    ENDGAME.style.display = "block"
+    var cong = setInterval(() =>{
+        if(grow <= 100 && shrink == 100){
+            ENDGAME.style.fontSize = `${grow}px`
+            grow += 5
+        }
+        else{
+            grow = 0
+            ENDGAME.style.fontSize = `${shrink}px`
+            shrink -= 5
+            if(shrink == 0){
+                shrink = 100
+            }
+        }
+    }, 55);
+    setTimeout(()=> {
+        clearInterval(cong)
+        ENDGAME.style.display = "none"
+        SCREEN.style.opacity = 1
+        SCREEN.style.pointerEvents = "auto"
+    }, 5000)
+    setInterval(() => {
+        CARD.className = "row bg-warning",
+        setTimeout(() => CARD.className = "row bg-success", 500)
+    }, 1000);
 }
 function setStyle(id, color, text=""){
     window.document.getElementById(id).innerHTML = `<div class="bg-${color} w-100 h-100 text-center">${text}</div>`
 }
 function startTimer(){
-    NUM.style.pointerEvents = "auto"
+    CARD.style.pointerEvents = "auto"
     start = setInterval(() => {
         seconds++
-        window.document.getElementById("time").innerHTML = `&#8987; <span onclick="gamePause()">${m0}${minuts}:${s0}${seconds} &#9656;</span>`
+        TIME.innerHTML = `&#8987; <span onclick="gamePause()">${m0}${minuts}:${s0}${seconds} &#9656;</span>`
         if(seconds == 9){
             s0 = ""
         }
@@ -95,11 +123,9 @@ function startTimer(){
             m0 = "0"
         }
     }, 1000)
-    pause = false
-    console.log(pause)
 }
 function gamePause(){
     clearInterval(start)
-    NUM.style.pointerEvents = "none"
-    window.document.getElementById("time").innerHTML = `&#8987; <span onclick="startTimer()">${m0}${minuts}:${s0}${seconds} &#9726;</span>`
+    CARD.style.pointerEvents = "none"
+    TIME.innerHTML = `&#8987; <span onclick="startTimer()">${m0}${minuts}:${s0}${seconds} &#9726;</span>`
 }
